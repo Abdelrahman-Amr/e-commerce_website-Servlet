32038e6629738f298949be22c2b10ec7895845c1
Abdelrahman-Amr <abdozyed2199@gmail.com>
2023-03-08 10:08:04 +0200

    Initial commit

---
+++ src/main/java/gov/iti/jets/servlet/MainServlet.java
@@ -0,0 +1,38 @@
+
+package gov.iti.jets.servlet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class MainServlet extends HttpServlet{
+    
+     @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        RequestDispatcher rd = req.getRequestDispatcher("/views/header.jsp");
+        rd.include(req, response);
+        
+        rd = req.getRequestDispatcher("/views/login.html");
+        rd.include(req, response);
+
+        rd = req.getRequestDispatcher("/views/footer.jsp");
+        rd.include(req, response);
+//        PrintWriter out = response.getWriter();
+ 
+    //    rd = req.getRequestDispatcher("Json");
+    //    rd.include(req, response);
+    }
+    
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        doGet(req, response);
+    }
+    
+}
+
---
+++ src/main/webapp/js/main.js
@@ -0,0 +1,13 @@
+function submit()
+{
+    
+    Swal.fire({
+        // position: 'top-end',
+        icon: 'success',
+        text:"Logged in successfully",
+        title: 'Success',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      
+}
