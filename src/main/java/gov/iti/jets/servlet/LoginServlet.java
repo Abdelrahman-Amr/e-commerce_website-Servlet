0ed453137f2ce8de95e9341ab3b4cb7b9f2231ce
Abdelrahman-Amr <abdozyed2199@gmail.com>
2023-03-09 11:29:59 +0200

    working on login

--- src/main/java/gov/iti/jets/servlet/LoginServlet.java
+++ src/main/java/gov/iti/jets/servlet/LoginServlet.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-public class MainServlet extends HttpServlet{
+public class LoginServlet extends HttpServlet{
     
      @Override
     public void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
@@ -24,15 +24,23 @@ public class MainServlet extends HttpServlet{
 
         rd = req.getRequestDispatcher("/views/footer.jsp");
         rd.include(req, response);
-//        PrintWriter out = response.getWriter();
- 
-    //    rd = req.getRequestDispatcher("Json");
-    //    rd.include(req, response);
     }
     
     @Override
     public void doPost(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
-        doGet(req, response);
+         response.setContentType("application/json");
+         String email = req.getParameter("email");
+         String password = req.getParameter("password");
+//        RequestDispatcher rd = req.getRequestDispatcher("/views/header.jsp");
+        PrintWriter writer = response.getWriter();
+
+        if(email.equalsIgnoreCase("abdo@iti.com") && password.equals("123"))
+        {
+            writer.write("1");
+        }else{
+            writer.write("0");
+
+        }
     }
     
 }
