38fca0ee7e644041f8739a8460136e168bdcbbd5
Abdelrahman-Amr <abdozyed2199@gmail.com>
2023-03-24 11:43:51 +0200

    fix thread local bug

--- src/main/java/gov/iti/jets/listener/ThreadLocalListener.java
+++ src/main/java/gov/iti/jets/listener/ThreadLocalListener.java
@@ -15,10 +15,10 @@ public class ThreadLocalListener implements ServletRequestListener {
     public void requestInitialized(ServletRequestEvent sre){
          MyLocal.getInstance().set(DBFactory.getInstance().createEntityManager());
 
-         CustomerService.getInstance().setManager(MyLocal.getInstance().get());
-         CategoryService.getInstance().setManager(MyLocal.getInstance().get());
-         ProductService.getInstance().setManager(MyLocal.getInstance().get());
-         SizeService.getInstance().setManager(MyLocal.getInstance().get());
+//         CustomerService.getInstance().setManager(MyLocal.getInstance().get());
+//         CategoryService.getInstance().setManager(MyLocal.getInstance().get());
+//         ProductService.getInstance().setManager(MyLocal.getInstance().get());
+//         SizeService.getInstance().setManager(MyLocal.getInstance().get());
     }
     @Override
     public void requestDestroyed(ServletRequestEvent sre){
